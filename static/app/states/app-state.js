7b4d77f64da38cff6b5a69389618397902d486b3
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2018-09-14 17:38:10 +0200

    use validOn handling as in orgviewer

--- static/app/states/app-state.js
+++ static/app/states/app-state.js
@@ -8,7 +8,14 @@ define([
               id: 'toolbar',
               hidden: true,
               cols: [
-                  {},
+                  { id: 'vt',
+                    view: 'datepicker',
+                    label: 'Visningstidspunkt:',
+                    labelWidth: 140,
+                    width: 280,
+                    stringResult: true,
+                    format: '%d-%m-%Y' },
+                  { gravity: 2 },
                   { id: 'session', type: 'header', borderless: true, template: '<span class="webix_icon icon fa-user-circle-o big_icon"></span><span class="user">#username#</span>' },
               ] },
             { $subview: true }
@@ -22,10 +29,20 @@ define([
             $ui: ui,
             $oninit: init
         },
-        querystringParameters: [ 'id' ]
+        querystringParameters: [ 'id', 'validOn' ],
+        activate: function(context) {
+            var validOn = basekit.setValidOn(new Date(context.parameters.validOn));
+            $$('vt').setValue(validOn);
+        }
     };
 
     function init() {
+        $$('vt').attachEvent('onChange', function(value) {
+            var validOn = helpers.dumpTimeZone(value);
+            stateRouter.go(null, // Go to same state with new validOn parameter
+                           { validOn: validOn.toISOString().slice(0,10) },
+                           { inherit: true });
+        });
         $$('session').parse({ username: basekit.username() });
 	$$('toolbar').show();
     }
