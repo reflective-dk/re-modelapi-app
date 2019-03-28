83b96ae2ca5e31a6755b6403a0675e13a096782c
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2019-03-28 13:27:06 +0100

    use simple template instead of textarea

--- static/app/states/csv.js
+++ static/app/states/csv.js
@@ -1,19 +1,19 @@
 define([
     'webix', 'common/$$', '../models/basekit'
 ], function(webix, $$, basekit) {
-    var areaId = webix.uid().toString();
+    var templateId = webix.uid().toString();
     return {
         name: 'csv',
         route: '/csv',
         template: {
-            $ui: { id: areaId, view: 'textarea' }
+            $ui: { id: templateId, view: 'template' }
         },
         querystringParameters: [ 'perspective' ],
         activate: function(context) {
             console.log(context);
             return basekit.fetchPerspective(context.parameters.perspective, true)
                 .then(function(csv) {
-                    $$(areaId).setValue(csv);
+                    $$(templateId).setHTML(csv);
                 })
                 .catch(console.log);
         }
