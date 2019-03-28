0c5456f1c277d5471d0b8331eddedec90d23aec9
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2019-03-28 14:03:48 +0100

    remove csv state which is no longer needed

--- static/app/states/init.js
+++ static/app/states/init.js
@@ -1,6 +1,6 @@
 define([
-    'webix', './state-router', './app-state', './class-diagram', './resource', './perspectives', './csv'
-], function(webix, stateRouter, appState, classDiagram, resource, perspectives, csv) {
+    'webix', './state-router', './app-state', './class-diagram', './resource', './perspectives'
+], function(webix, stateRouter, appState, classDiagram, resource, perspectives) {
   return function(initialState, defaultState) {
     defaultState = defaultState || initialState;
     webix.ready(function() {
@@ -8,7 +8,6 @@ define([
       stateRouter.addState(classDiagram);
       stateRouter.addState(resource);
       stateRouter.addState(perspectives);
-      stateRouter.addState(csv);
       stateRouter.on('routeNotFound', function(route, parameters) {
         console.log('route not found', route, defaultState);
         stateRouter.go(defaultState, { route: route });
